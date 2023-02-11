510c0d3f5b56689a7e672f247cd8d731eeecf7b2
andrii.baranchuk <andreybaranchuk98@gmail.com>
2023-02-11 22:56:08 +0200

    Implemented SnakeTail.

---
+++ Assets/_ROOT/Scripts/Snake/SnakeTail.cs
@@ -0,0 +1,43 @@
+﻿namespace SnakeRunner.Gameplay.Snake
+{
+    using UnityEngine;
+
+    public class SnakeTail : MonoBehaviour
+    {
+        [SerializeField]
+        private float movementSpeed;
+
+        [SerializeField]
+        private Transform target;
+
+        private Vector3 lastPosition;
+        
+        private bool targetIsMoving;
+
+        private void Start()
+        {
+            lastPosition = target.position;
+        }
+
+        private void FixedUpdate()
+        {
+            CheckIfTargetIsMoving();
+            
+            if(targetIsMoving)
+            {
+                var direction = target.position - transform.position;
+                transform.Translate(direction * (movementSpeed * Time.fixedDeltaTime));
+                transform.LookAt(target);
+            }
+        }
+        
+        private void CheckIfTargetIsMoving()
+        {
+            var position = target.position;
+
+            targetIsMoving = (position - lastPosition).sqrMagnitude >= float.Epsilon;
+
+            lastPosition = position;
+        }
+    }
+}
