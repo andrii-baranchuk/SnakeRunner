539066aeb66fe79e221f49422f339efc687ad209
andrii.baranchuk <andreybaranchuk98@gmail.com>
2023-02-11 21:43:52 +0200

    Added CinemachineExtension to LockX.

---
+++ Assets/_ROOT/Scripts/Cinemachine/LockCameraX.cs
@@ -0,0 +1,25 @@
+﻿namespace Cinemachine
+{
+    using UnityEngine;
+    using UnityEngine.Serialization;
+
+    [ExecuteInEditMode] [SaveDuringPlay] [AddComponentMenu("")] // Hide in menu
+    public class LockCameraX : CinemachineExtension
+    {
+        [FormerlySerializedAs("m_ZPosition")]
+        [Tooltip("Lock the camera's Z position to this value")]
+        public float m_XPosition;
+ 
+        protected override void PostPipelineStageCallback(
+            CinemachineVirtualCameraBase vcam,
+            CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
+        {
+            if (stage == CinemachineCore.Stage.Body)
+            {
+                var pos = state.RawPosition;
+                pos.x = m_XPosition;
+                state.RawPosition = pos;
+            }
+        }
+    }
+}
