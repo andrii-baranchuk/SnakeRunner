b744c4035b6d59930383f555eefd2c9fb33ee5c1
andrii.baranchuk <andreybaranchuk98@gmail.com>
2023-02-27 21:42:03 +0200

    Added Collision and Trigger.

---
+++ Assets/_ROOT/Scripts/Collisions/Collision.cs
@@ -0,0 +1,30 @@
+﻿namespace Collisions
+{
+    using System;
+    using UnityEngine;
+
+    public abstract class Collision<T> : MonoBehaviour
+    {
+        public event Action<T> OnEnter;
+        public event Action<T> OnStay; 
+        public event Action<T> OnExit;
+
+        protected virtual void OnCollisionEnter(Collision other)
+        {
+            if(other.gameObject.TryGetComponent(out T component))
+                OnEnter?.Invoke(component);
+        }
+
+        protected virtual void OnCollisionStay(Collision other)
+        {
+            if(other.gameObject.TryGetComponent(out T component))
+                OnStay?.Invoke(component);
+        }
+
+        protected virtual void OnCollisionExit(Collision other)
+        {
+            if(other.gameObject.TryGetComponent(out T component))
+                OnExit?.Invoke(component);
+        }
+    }
+}
