d8ffbe2a260b3762f438bd5ed87be4c56b4ff49c
andrii.baranchuk <andreybaranchuk98@gmail.com>
2023-03-12 18:56:58 +0200

    Setuped LevelFailing.

--- Assets/_ROOT/Scripts/Gameplay/Level/Level.cs
+++ Assets/_ROOT/Scripts/Gameplay/Level/Level.cs
@@ -1,9 +1,33 @@
 ﻿namespace SnakeRunner.Gameplay.Level
 {
+    using Infrastructure.ServiceLocator;
+    using UI;
+    using Unit.Death;
     using UnityEngine;
 
     public class Level : MonoBehaviour
     {
         public TrailsContainer Trails;
+
+        public UnitDeath Unit;
+
+        private IUIBuilder uiBuilder;
+
+        private void Start()
+        {
+            uiBuilder = AllServices.Container.Single<IUIBuilder>();
+            
+            Unit.OnDeath += FailLevel;
+        }
+
+        private void FailLevel()
+        {
+            uiBuilder.CreateWindow<LevelFailedWindow>();
+        }
+
+        private void OnDestroy()
+        {
+            Unit.OnDeath -= FailLevel;
+        }
     }
 }
