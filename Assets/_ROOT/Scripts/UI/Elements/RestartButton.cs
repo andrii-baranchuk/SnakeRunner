fef2d42bab6a395840e89b010522b6048aa328ad
andrii.baranchuk <andreybaranchuk98@gmail.com>
2023-03-12 18:56:46 +0200

    Added LevelFailedWindow.

---
+++ Assets/_ROOT/Scripts/UI/Elements/RestartButton.cs
@@ -0,0 +1,33 @@
+﻿namespace UI
+{
+    using UnityEngine;
+    using UnityEngine.SceneManagement;
+    using UnityEngine.UI;
+
+    [RequireComponent(typeof(Button))]
+    public class RestartButton : MonoBehaviour
+    {
+        [SerializeField]
+        private Button button;
+
+        private void OnValidate()
+        {
+            button = GetComponent<Button>();
+        }
+
+        private void Awake()
+        {
+            button.onClick.AddListener(Restart);
+        }
+
+        private void Restart()
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        private void OnDestroy()
+        {
+            button.onClick.RemoveListener(Restart);
+        }
+    }
+}
---
+++ Assets/_ROOT/Scripts/UI/Windows/Window.cs
@@ -0,0 +1,13 @@
+﻿namespace UI
+{
+    using System;
+    using UnityEngine;
+
+    public abstract class Window : MonoBehaviour
+    {
+        protected virtual void OnValidate()
+        {
+            gameObject.name = GetType().Name;
+        }
+    }
+}
